8365b5a953962ee6528bbe3a2c00c698154e3bf4
nexton97 <majidblid@gmail.com>
2023-03-27 02:14:33 +0200

    add countries list - fixing datagrid

--- src/components/DataBox/databox.tsx
+++ src/components/DataBox/databox.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { MilitaryTech } from '@mui/icons-material';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,9 +18,30 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#2196f3',
     
   },
+  bold: {
+    fontWeight: 'bold',
+  },
+  bigger: {
+    fontSize: '1.5rem',
+  },
+  gold: {
+    color: 'gold',
+    fontWeight: 'bold',
+    fontSize: '1.3rem'
+  },
+  silver: {
+    color: 'silver',
+    fontWeight: 'bold',
+    fontSize: '1.3rem'
+  },
+  bronze: {
+    color: '#fcca7e',
+    fontWeight: 'bold',
+    fontSize: '1rem'
+  }
 }));
 
-export default function TopUsers({ totalUsers, highestScore, top3Users }: { totalUsers: number, highestScore: number, top3Users: Array<{name: string, score: number}> }) {
+export default function TopUsers({ totalUsers, highestScore, top3Users }: { totalUsers: number, highestScore: number, top3Users: Array<{username: string, points: number}> }) {
   const classes = useStyles();
 
   return (
@@ -28,25 +50,26 @@ export default function TopUsers({ totalUsers, highestScore, top3Users }: { tota
         <Grid item xs={4}>
           <Paper className={classes.paper}>
             <Typography variant="h5">Total Players</Typography>
-            <Typography>{totalUsers}</Typography>
+            <Typography className={`${classes.bold} ${classes.bigger}`}>{totalUsers}</Typography>
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
             <Typography variant="h5">Top Score</Typography>
-            <Typography>{highestScore}</Typography>
+            <Typography className={`${classes.bold} ${classes.bigger}`}>{highestScore}</Typography>
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
             <Typography variant="h5">Top 3 Scores</Typography>
             {top3Users.map((user, index) => (
-            <Typography key={index}>{user.name} - {user.score}</Typography>
+              <Typography key={index} className={index === 0 ? classes.gold : index === 1 ? classes.silver : classes.bronze}>
+                {user.username} - {user.points} {index === 0 && <MilitaryTech fontSize="small" />}
+              </Typography>
             ))}
           </Paper>
         </Grid>
       </Grid>
     </div>
-);
+  );
 }
-
